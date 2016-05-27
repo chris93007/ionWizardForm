70f1afc242f6c9dde1a24392c84c42f825b347e0
Christine <christine.john@people10.com>
2016-05-27 11:19:07 +0530

    addExpense

--- www/templates/addNewExpense/addNewExpense.js
+++ www/templates/addNewExpense/addNewExpense.js
@@ -1,27 +1,18 @@
 angular.module('starter.controllers')
 
-.controller('addNewExpenseCtrl', ['$scope', '$state','$http','baseUrl','$filter', function($scope, $state,$http,baseUrl,$filter) {
+.controller('addNewExpenseCtrl', ['$scope', '$state','$http','baseUrl','$filter','$log','$q','$timeout', function($scope, $state,$http,baseUrl,$filter,$log,$q,$timeout) {
 
 
     $scope.expenseInfo={};
     var params={};
-    $scope.toDisplay={};
+
     $scope.payby=new Date();
-    $scope.allcategories = [];
-    $scope.selectedCategory = [];
-    $scope.selectedFrequency = [];
-    $scope.allFrequencies = [];
+
     var allOptions=[];
 
-    $http.get(baseUrl+"categories").success(function (data) {
-        allOptions=data.categories;
-        allcategories=data.categories;
+    $http.get(baseUrl+"frequency").success(function (data) {
+      $scope.frequencies=data.frequency;
     });
-
-
-    // $http.get(baseUrl+"frequency").success(function (data) {
-    //   console.log(data);
-    // });
 
     function formatDate(date) {
       var d = new Date(date || Date.now()),
@@ -49,8 +40,6 @@ angular.module('starter.controllers')
       params.noofinstances=$scope.expenseInfo.noofinstances;
       params.amount=$scope.expenseInfo.amount;
       params.payby=formatDate($scope.expenseInfo.payby);
-      // params.payby=$scope.expenseInfo.payby;
-      // formatDate(params.payby);
       console.log(params);
       $http.post(baseUrl+"api/expenses",params).success(function(data){
         console.log(data);
@@ -63,53 +52,77 @@ angular.module('starter.controllers')
     };
 
 
-    $scope.$watchCollection('[expenseInfo.category]', function () {
-        if($scope.expenseInfo.category){
-          $http.get(baseUrl+"frequency").success(function (data) {
-            allOptions=data.frequency;
-            allFrequencies=data.frequency;
-          });
-        }
-    });
+  // Autocomplete
 
-   $scope.queryGroups = function(search) {
-       var firstPass = $filter('filter')(allOptions, {title:search});
-       return firstPass.filter(function(item) {
-           return $scope.selectedCategory.indexOf(item) === -1;
-       });
-   };
+   $scope.simulateQuery = false;
+   $scope.isDisabled    = false;
+   $scope.selectedItem;
 
-   $scope.queryGroups = function(search) {
-       var firstPass = $filter('filter')(allOptions, {title:search});
-       return firstPass.filter(function(item) {
-           return $scope.selectedFrequency.indexOf(item) === -1;
-       });
-   };
 
-   $scope.$watchCollection('selectedCategory', function() {
-       $scope.availableGroups = $scope.queryGroups('');
-       if($scope.selectedCategory[0]){
-          $scope.expenseInfo.category=$scope.selectedCategory[0];
-          $scope.disable=true;
-       }
-       else{
-         delete   $scope.expenseInfo.category;
-         $scope.disable=false;
-         allOptions=allcategories;
-       }
+   $scope.options=[];
 
+   $http.get(baseUrl+"categories").success(function (data) {
+     for(var i=0;i<data.categories.length;i++){
+       data.categories[i].lowercase=data.categories[i].title.toLowerCase();
+     }
+     $scope.options=data.categories;
+      console.log($scope.options);
    });
 
-   $scope.$watchCollection('selectedFrequency', function() {
-       $scope.availableGroups = $scope.queryGroups('');
-       if($scope.selectedFrequency[0]){
-            $scope.expenseInfo.frequency=$scope.selectedFrequency[0];
-            $scope.disable2=true;
-       }else{
-           delete   $scope.expenseInfo.category;
-           $scope.disable2=false;
-       }
+   $scope.querySearch   = querySearch;
+   $scope.selectedItemChange = selectedItemChange;
+   $scope.searchTextChange   = searchTextChange;
+   $scope.searchText;
 
-   });
+   $scope.newState = newState;
+
+   function newState(state) {
+     alert("New Category to be Added");
+   }
+
+   // ******************************
+   // Internal methods
+   // ******************************
+
+   /**
+    * Search for states... use $timeout to simulate
+    * remote dataservice call.
+    */
+   function querySearch (query) {
+     var results = query ? $scope.options.filter( createFilterFor(query) ) : $scope.options,
+         deferred;
+     if ($scope.simulateQuery) {
+       deferred = $q.defer();
+       $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+       return deferred.promise;
+     } else {
+       return results;
+     }
+   }
+
+   function searchTextChange(text) {
+     $log.info('Text changed to ' + text);
+   }
+
+   function selectedItemChange(item) {
+     $log.info('Item changed to ' + JSON.stringify(item));
+   }
+
+   /**
+    * Build `states` list of key/value pairs
+    */
+
+   /**
+    * Create filter function for a query string
+    */
+   function createFilterFor(query) {
+     var lowercaseQuery = angular.lowercase(query);
+
+     return function filterFn(state) {
+       return (state.lowercase.indexOf(lowercaseQuery) === 0);
+     };
+
+   }
+
 
 }]);
