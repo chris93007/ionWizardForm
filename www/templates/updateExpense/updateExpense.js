d9b037d406b635ec15d3c6e02df7c7e5cdb5c644
Christine <christine.john@people10.com>
2016-05-27 11:20:38 +0530

    addexp

--- www/templates/updateExpense/updateExpense.js
+++ www/templates/updateExpense/updateExpense.js
@@ -1,16 +1,40 @@
 angular.module('starter.controllers')
 
 .controller('updateExpenseCtrl', ['$scope','$http','baseUrl','$stateParams','$state', function($scope,$http,baseUrl,$stateParams,$state) {
-
+ 
+ 	//console.log("IN updateExpenseCTrl")
 
   $http.get(baseUrl+'api/expenses/'+$stateParams.expenseId).success(function(data){
-    console.log(data);
-    $scope.data=data;
+    //console.log(data);
+     
+    //$scope.category=data.category.data;
+
+  	 $scope.data=data;
+     console.log($scope.data);
+     $scope.id=data.id;
+     console.log($scope.id); 	
+
+
+    $scope.backToExpenses = function(){
+        $scope.expenseInfo={};
+        $state.go('app.expenses');
+    };
+
+    $scope.updateExpense =function(id){
+    	console.log("In updateExp");
+   	    console.log("id:" +id);
+   	    $state.go('app.updateExpense',{expenseId:id});
+   	    console.log("$scope.id:" +$scope.id);
+
+   	    //$scope.data= data;
+   	   
+
+    };
+
   });
 
-  $scope.cancel = function() {
-      $scope.expenseInfo={};
-      $state.go('app.expenses');
-  };
+// api :
 
-}]);
+}
+
+]);
